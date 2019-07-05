356eb130160294234692c2a1287bdf498740675e
Jamie Hampton <jhampton@agrilyst.com>
2019-07-05 16:11:21 -0400

    lol owenbots first easter egg

--- owenbot/commands/responses.rb
+++ owenbot/commands/responses.rb
@@ -4,6 +4,10 @@ module Owenbot
       command 'thank you', 'thanks' do |client, data, _match|
         client.say(channel: data.channel, text: "You're welcome!")
       end
+
+      scan('real magic') do |client, data, stocks|
+        client.say(channel: data.channel, text: "I'm not a stage magician, Carolyn.")
+      end
     end
   end
 end
