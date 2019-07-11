69576dd4759c4616fa99f169e92e9a7839ad106a
Jamie Hampton <jhampton@agrilyst.com>
2019-07-11 18:33:58 -0400

    added image and new alias

--- owenbot/bot.rb
+++ owenbot/bot.rb
@@ -1,7 +1,7 @@
 module Owenbot
   class Bot < SlackRubyBot::Bot
     SlackRubyBot.configure do |config|
-      config.aliases = ['okay-owen', 'ok-owen']
+      config.aliases = ['okay-owen', 'ok-owen', 'overwatch']
     end
   end
 end
