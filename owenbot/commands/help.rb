aa56f59ff87f0d94a9ab3a901110d3360250fd86
Jamie Hampton <jhampton@agrilyst.com>
2019-07-10 20:55:28 -0400

    slightly more robust help command

--- owenbot/commands/help.rb
+++ owenbot/commands/help.rb
@@ -2,7 +2,11 @@ module Owenbot
   module Commands
     class Help < SlackRubyBot::Commands::Base
       command 'help' do |client, data, _match|
-        client.say(channel: data.channel, text: 'I do not have a help guide set up yet. You are on your own.', gif: 'help')
+        help_text = "My name is Owenbot! I will try to help answer your questions for you! \n\n"
+        help_text += "If you want me to look up some information for you, say _owenbot tell me about [your query]_ \n\n"
+        help_text += "I can help you practice your greek! Say _owenbot how do you say [the word you'd like to translate]_ \n\n"
+        help_text += "I'm still a baby and I don't know a lot of phrases yet, but I'm still learning!"
+        client.say(channel: data.channel, text: help_text, gif: 'help')
       end
     end
   end
